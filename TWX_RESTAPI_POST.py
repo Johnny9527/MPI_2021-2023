a93af52e7dfc5f1dd6065364f862731b2136b133
Chia-Yu Lee <62075542+Johnny9527@users.noreply.github.com>
2022-03-28 15:23:35 +0100

    Update TWX_RESTAPI_POST.py

--- TWX_RESTAPI_POST.py
+++ TWX_RESTAPI_POST.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-@author: johnny.lee
-"""
 import requests
 import json
 
@@ -9,21 +5,10 @@ url = 'https://mpiuktw01.mpiuk.com:8443/Thingworx'
 
 header = {'Content-Type': 'application/json', 'appKey': 'f680a2c3-6cef-43d2-96ef-2a9294603f0e'}
 
-# Create new Thing.
-# addNewThing = {'name': 'NewThing_1',
-#                'description': 'Create by code',
-#                'thingTemplateName': 'GenericThing'}
-#
-# response = requests.put(url + '/Resources/EntityServices/Services/CreateThing', headers=header, json=addNewThing, verify=False)
+# Add multiple new property into specific Thing.
+addMultipleProperty = {"name": "test99", "description": "POST by RESTAPI_appkey", "type": "NUMBER", "persistent": "True", "defaultValue": "100"}
 
-# Add new property into specific Thing.
-addNewProperty = {'name': 'test2',
-                  'description': 'POST from code',
-                  'type': 'NUMBER',
-                  'persistent': 'True',
-                  'defaultValue': '999'}
-
-response = requests.put(url + '/Things/APItestThing/Services/AddPropertyDefinition', headers=header, json=addNewProperty, verify=False)
+response = requests.post(url + '/Things/APItestThing/Services/AddPropertyDefinitions', headers=header, data=addMultipleProperty, verify=False)
 
 print(response.status_code)
 print(response.text)
