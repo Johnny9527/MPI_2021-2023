2a4760574f2359ed4c10db5575825a96865ec5f6
Chia-Yu Lee <62075542+Johnny9527@users.noreply.github.com>
2022-03-28 15:24:12 +0100

    Update TWX_RESTAPI_GET.py

--- TWX_RESTAPI_GET.py
+++ TWX_RESTAPI_GET.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-# ThingWorx API - GET
 url =  'https://mpiuktw01.mpiuk.com:8443/Thingworx'
+
 header = {'Accept': 'application/json', 'appKey': 'f307c5ef-9022-4aae-ae62-ce55dedbfdc9'}
 
 response = requests.get(url + '/Things/APItestThing/Properties/testVariable', headers=header, verify=False)
