48a1919077446e26d63d6655e4f88b4b1e9c15cd
Chia-Yu Lee <62075542+Johnny9527@users.noreply.github.com>
2022-03-28 15:19:48 +0100

    Update and rename TWX_RESTAPI.py to TWX_RESTAPI_GET.py

--- TWX_RESTAPI_GET.py
+++ TWX_RESTAPI_GET.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 26 14:31:39 2021
-
-@author: chris.paterson
-"""
-
 import requests
 import json
 
+# ThingWorx API - GET
 url =  'https://mpiuktw01.mpiuk.com:8443/Thingworx'
 header = {'Accept': 'application/json', 'appKey': 'f307c5ef-9022-4aae-ae62-ce55dedbfdc9'}
 
